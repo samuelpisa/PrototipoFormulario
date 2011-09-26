efa0f6f256b2ad4a023069bf1e87ca9c43bda514
Igor Alves <igorsoaresalves@gmail.com>
2011-09-26 20:31:48 -0300

    Provider inserido
    Precisei subir esses arquivos pra baixar o projeto em outra pasta, pq
    ta dando pau aqui!

---
+++ src/com/providers/FormulariosDataProvider.java
@@ -0,0 +1,67 @@
+package com.providers;
+
+import android.content.ContentProvider;
+import android.net.Uri;
+import android.content.ContentValues;
+import android.database.Cursor;
+
+public class FormulariosDataProvider extends ContentProvider {
+	public static final Uri CONTENT_URI = Uri
+			.parse("content://com.providers.formulariosdataprovider");
+
+	/**
+	 * @see android.content.ContentProvider#delete(Uri,String,String[])
+	 */
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// TODO Put your code here
+		return 0;
+	}
+
+	/**
+	 * @see android.content.ContentProvider#getType(Uri)
+	 */
+	@Override
+	public String getType(Uri uri) {
+		// TODO Put your code here
+		return null;
+	}
+
+	/**
+	 * @see android.content.ContentProvider#insert(Uri,ContentValues)
+	 */
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Put your code here
+		return null;
+	}
+
+	/**
+	 * @see android.content.ContentProvider#onCreate()
+	 */
+	@Override
+	public boolean onCreate() {
+		// TODO Put your code here
+		return false;
+	}
+
+	/**
+	 * @see android.content.ContentProvider#query(Uri,String[],String,String[],String)
+	 */
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO Put your code here
+		return null;
+	}
+
+	/**
+	 * @see android.content.ContentProvider#update(Uri,ContentValues,String,String[])
+	 */
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Put your code here
+		return 0;
+	}
+}
