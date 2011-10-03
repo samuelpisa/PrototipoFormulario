99d1dad73a135a5b4b970d10220fdbb8ff63946c
Igor Alves <igorsoaresalves@gmail.com>
2011-10-03 20:49:38 -0300

    Revert "DbHelper e exemplo de insert na questao1; ACHO MELHOR COLOCAR TODAS AS QUESTOES NO BANCO… DEPOIS EXPLICO…."
    This reverts commit 29194d3fab8ec91f3e557ca94a04416fd648aff3.

--- src/com/prototipo/DbHelper.java
+++ src/com/prototipo/DbHelper.java
@@ -10,19 +10,10 @@ public class DbHelper extends SQLiteOpenHelper {
 	
 	private static final String TAG = DbHelper.class.getSimpleName();
 	public static final String DB_NAME = "forms.db";
-	public static final int DB_VERSION = 11; 
-	
-	
+	public static final int DB_VERSION = 1; 
 	public static final String TABLE_QUESTIONS = "questions";
 	public static final String QUESTION_ID = BaseColumns._ID;
 	public static final String QUESTION_DESC= "description";
-	
-	
-	public static final String TABLE_ALTERNATIVES = "alternatives";
-	public static final String ALTERNATIVE_ID = BaseColumns._ID;
-	public static final String ALTERNATIVE_DESC= "description";
-	public static final String ALTERNATIVE_QUESTION_ID= "idQuestion";
-	
 	
 	private Context context;
 	
@@ -33,47 +24,19 @@ public class DbHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		String sql = String.format("create table %s(%s integer primary key autoincrement, %s text);", TABLE_QUESTIONS,QUESTION_ID,QUESTION_DESC);
-		Log.d(TAG,"onCreate sql1: "+sql);
+		String sql = "create table questions(_id int primary key, desc text);";
+		sql += "create table alternatives(_id int primary key, idQuestions int, desc text);";
+		Log.d(TAG,"onCreate sql: "+sql);
 		db.execSQL(sql);
-		
-		sql = String.format(" create table %s(%s integer primary key autoincrement, %s text, %s int);", TABLE_ALTERNATIVES,ALTERNATIVE_ID,ALTERNATIVE_DESC,ALTERNATIVE_QUESTION_ID);
-		Log.d(TAG,"onCreate sql2: "+sql);
-		db.execSQL(sql);
-		
-		
-
-		
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		String sql = String.format("drop table if exists %s;", TABLE_QUESTIONS);
+		String sql = "drop table if exists"+ TABLE_QUESTIONS;
 		db.execSQL(sql);
 		Log.d(TAG,"onUpgrade sql: "+sql);
-		
-		sql = String.format("drop table if exists %s;", TABLE_ALTERNATIVES);
-		db.execSQL(sql);
-		Log.d(TAG,"onUpgrade sql: "+sql);
-		
 		this.onCreate(db);
 		
 	}
-	
-	public void truncateDB(SQLiteDatabase db) {
-		String sql = String.format("DELETE FROM %s;", TABLE_QUESTIONS);
-		db.execSQL(sql);
-		Log.d(TAG,"!!!!!!!!!!!!!!!!!!!!onUpgrade sql: "+sql);
-		
-		sql = String.format("DELETE FROM %s; ", TABLE_ALTERNATIVES);
-		db.execSQL(sql);
-		Log.d(TAG,"onTRUNCATE sql: "+sql);
-	}
-	
-	public void insert(){
-		
-	}
-	
-
 
 }
